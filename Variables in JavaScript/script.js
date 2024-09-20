7fe755b23361a8c02982e784d84ec201f0106a09
Vrs <137067506+VRS-Z@users.noreply.github.com>
2024-09-20 20:27:41 -0300

    Add files via upload

---
+++ Variables in JavaScript/script.js
@@ -0,0 +1,15 @@
+let name = "John";
+
+let admin;
+
+admin = name;
+
+alert(admin);
+
+let planetEarth = "World";
+
+let userData = "John";
+
+const BIRTHDAY = "18.04.1982";
+
+const age = someCode(BIRTHDAY);
