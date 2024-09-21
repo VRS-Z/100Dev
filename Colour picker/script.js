f580d7f8c56d23b88f4590a897fcfd3dc935f3a7
Vrs <137067506+VRS-Z@users.noreply.github.com>
2024-09-21 17:22:07 -0300

    Add files via upload

---
+++ Colour picker/script.js
@@ -0,0 +1,9 @@
+function chargerForBlue() {
+  let body = document.body;
+  body.style.background = "blue";
+}
+
+function chargerForRed() {
+  let body = document.body;
+  body.style.background = "red";
+}
