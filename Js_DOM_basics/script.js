ab26a3efabd9693768007deac44dd9ed26581bfd
Vrs <137067506+VRS-Z@users.noreply.github.com>
2024-10-01 10:07:59 -0300

    Add files via upload

---
+++ Js_DOM_basics/script.js
@@ -0,0 +1,30 @@
+// document.getElementById("demo").innerHTML = "Hello World!";
+// const element = document.getElementById("intro");
+
+// document.getElementById("demo").innerHTML =
+//   "The text from the intro paragraph is: " + element.innerHTML;
+// const element = document.getElementsByTagName("p");
+
+// document.getElementById("demo").innerHTML =
+//   "The text in first paragraph (index 0) is: " + element[0].innerHTML;
+
+// const x = document.getElementById("main");
+// const y = x.getElementsByTagName("p");
+
+// document.getElementById("demo").innerHTML =
+//   'The first paragraph (index 0) inside "main" is: ' + y[0].innerHTML;
+// const x = document.getElementsByClassName("intro");
+// document.getElementById("demo").innerHTML =
+// 'The first paragraph (index 0) with class="intro" is: ' + x[0].innerHTML;
+// const x = document.querySelectorAll("p.intro");
+// document.getElementById("demo").innerHTML =
+// 'The first paragraph (index 0) with class="intro" is: ' + x[0].innerHTML;
+
+// const x = document.forms["frm1"];
+// let text = "";
+// for (let i = 0; i < x.length; i++) {
+//   text += x.elements[i].value + "<br>";
+// }
+// document.getElementById("demo").innerHTML = text;
+
+// document.getElementById("demo").innerHTML = "Date :" + Date();
