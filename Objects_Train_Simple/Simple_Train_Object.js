a48530bd5c1aa6f3d01d31af11d496e8934ec1f0
Vrs <137067506+VRS-Z@users.noreply.github.com>
2024-11-17 16:43:01 -0300

    add new exercise about object

--- Objects_Train_Simple/Simple_Train_Object.js
+++ Objects_Train_Simple/Simple_Train_Object.js
@@ -33,3 +33,118 @@ function multiplyNumeric(obj) {
     }
   }
 }
+
+// My example of simple object :)
+
+let soulsLike = {
+  init: "demonSouls",
+  mid: "darksouls 3",
+  peak: "elden Ring",
+};
+
+soulsLike.motivation = function () {
+  alert("Go to farm xp!");
+};
+
+soulsLike.mvp = "sekiro";
+soulsLike.badGame = "amored Core";
+delete soulsLike.badGame;
+
+console.log(soulsLike);
+
+// End my fun
+
+function makeUser() {
+  return {
+    name: "John",
+    ref: this,
+  };
+}
+
+let user = makeUser();
+
+alert(user.ref.name); // What's the result?
+// The result is: "John", alert tell the prop this.name in the function makeUser.
+// Bad, i miss. This element is one error! (In the situation bellow).
+
+// let calculator = {
+//   sum() {
+//     return this.a + this.b;
+//   },
+
+//   mul() {
+//     return this.a * this.b;
+//   },
+
+//   read() {
+//     this.a = +prompt("a?", 0);
+//     this.b = +prompt("b?", 0);
+//   },
+// };
+
+// calculator.read();
+// alert(calculator.sum());
+// alert(calculator.mul());
+
+let ladder = {
+  step: 0,
+  up() {
+    this.step++;
+    return this;
+  },
+  down() {
+    this.step--;
+    return this;
+  },
+  showStep() {
+    alert(this.step);
+    return this;
+  },
+};
+
+function A() { ... }
+function B() { ... }
+
+let a = new A();
+let b = new B();
+
+alert( a == b ); // true
+// I think that no.
+// Miss again!
+
+function Calculator() {
+
+  this.read = function() {
+    this.a = +prompt('a?', 0);
+    this.b = +prompt('b?', 0);
+  };
+
+  this.sum = function() {
+    return this.a + this.b;
+  };
+
+  this.mul = function() {
+    return this.a * this.b;
+  };
+}
+
+
+let calculator = new Calculator();
+calculator.read();
+
+alert( "Sum=" + calculator.sum() );
+alert( "Mul=" + calculator.mul() );
+
+function Accumulator(startingValue) {
+  this.value = startingValue;
+
+  this.read = function() {
+    this.value += +prompt('How much to add?', 0);
+  };
+
+}
+
+let accumulator = new Accumulator(1);
+accumulator.read();
+accumulator.read();
+alert(accumulator.value);
